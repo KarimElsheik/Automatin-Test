32e3c8e7fad4a9e678af1daa1f1dd6fb7ee3d8b9
karim.alsheikh <karim.alsheikh@gizasystems.onmicrosoft.com>
2025-01-02 05:07:29 +0200

    .

--- src/test/java/automationExercise/tests/Automation.java
+++ src/test/java/automationExercise/tests/Automation.java
@@ -20,7 +20,7 @@ public class Automation {
     WebDriver driver;
     private JsonFileManager testData;
 
-
+// mnnbolkj
 
     @Test
     @Description("Sign up successfully and delete the account")
@@ -87,6 +87,6 @@ public class Automation {
     public void Properties() {
         PropertiesReader.loadProperties();
     }
-    //
+    //[poihgj
 
 }
